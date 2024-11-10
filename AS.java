3d9b3c16d668e372b1e6dc5a9505cde4b2e74d12
yehonatan gutman <gutman.yehonatan@post.runi.ac.il>
2024-11-10 13:53:22 +0200

    done

---
+++ AS.java
@@ -0,0 +1,14 @@
+
+
+public class AS {
+    public static void main(String[] args) {
+        String city = "haifa";
+        int age = 18;
+        double money = 2.0;
+        String yy = args[0];
+        System.out.println(".(gutman");
+        
+
+    }
+
+}
--- Bill3.java
+++ Bill3.java
@@ -1,8 +1,12 @@
-// Splits a restaurant bill evenly among three diners.
 public class Bill3 {
-	public static void main(String[] args) {
-		// To get you started, here is the first line in the program:
-	    String name1 = args[0];
-	    // Replace this comment with the rest of your code   
-	}
+    public static void main(String[] args) {
+    
+        String name1 = args[0];
+        String name2 = args[1];
+        String name3 = args[2];
+        int bill = Integer.parseInt(args[3]); 
+        double personalBill = bill; 
+        personalBill = Math.ceil(personalBill / 3); 
+        System.out.println("Great! " + name1 + " , " + name2 + " and " + name3 + " pay " + personalBill + " each");
+    }
 }
